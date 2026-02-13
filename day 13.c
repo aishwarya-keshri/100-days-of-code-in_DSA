9bbd7de0645c8a98eb32129535977bec68b1c7a7
aishwarya keshri <Aishwarya.21666@stu.upes.ac.in>
2026-02-13 22:33:23 +0530

    Add files via upload

---
+++ day 13.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    scanf("%d %d", &r, &c);
+
+    int matrix[r][c];
+
+    // Input matrix
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    int top = 0, bottom = r - 1;
+    int left = 0, right = c - 1;
+
+    while(top <= bottom && left <= right) {
+
+        // Traverse Left to Right
+        for(int i = left; i <= right; i++) {
+            printf("%d ", matrix[top][i]);
+        }
+        top++;
+
+        // Traverse Top to Bottom
+        for(int i = top; i <= bottom; i++) {
+            printf("%d ", matrix[i][right]);
+        }
+        right--;
+
+        // Traverse Right to Left
+        if(top <= bottom) {
+            for(int i = right; i >= left; i--) {
+                printf("%d ", matrix[bottom][i]);
+            }
+            bottom--;
+        }
+
+        // Traverse Bottom to Top
+        if(left <= right) {
+            for(int i = bottom; i >= top; i--) {
+                printf("%d ", matrix[i][left]);
+            }
+            left++;
+        }
+    }
+
+    return 0;
+}
